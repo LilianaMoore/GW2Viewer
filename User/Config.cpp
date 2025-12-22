6723eebe7e4a1f283d1ffbe4fa2b19216c0b1fbd
xvwyh <43143822+xvwyh@users.noreply.github.com>
2025-12-22 12:45:03 +0200

    Added notifications when saving and loading config

--- User/Config.cpp
+++ User/Config.cpp
@@ -1,24 +1,49 @@
 ﻿module GW2Viewer.User.Config;
 import GW2Viewer.Common.JSON;
+import GW2Viewer.UI.Notifications;
 
-static std::filesystem::path const path = R"(.\config.json)";
+static std::filesystem::path const configPath = R"(.\config.json)";
 
 namespace GW2Viewer::User
 {
 
 void Config::Load()
 {
-    auto contents = (std::stringstream() << std::ifstream(path).rdbuf()).str();
+    auto contents = (std::stringstream() << std::ifstream(configPath).rdbuf()).str();
     if (contents.empty())
         return;
     from_json(json::parse(contents, nullptr, false), *this);
     FinishLoading();
+    G::Notifications.AddTimed(1s, {
+        .Type = GW2Viewer::UI::Notification::Types::Success,
+        .Text = "Config loaded.",
+    });
 }
 void Config::Save()
 {
     std::erase_if(ContentNamespaceNames, [](auto const& pair) { return pair.second.empty(); });
     std::erase_if(ContentObjectNames, [](auto const& pair) { return pair.second.empty(); });
-    std::ofstream(path) << ordered_json(*this).dump(2);
+    std::ofstream file(configPath);
+    if (!file.is_open())
+    {
+        G::Notifications.AddCloseable({
+            .Type = GW2Viewer::UI::Notification::Types::Error,
+            .Text = "Config save failed:\nUnable to open the destination file for writing.",
+        });
+        return false;
+    }
+    if (!(file << ordered_json(*this).dump(2)))
+    {
+        G::Notifications.AddCloseable({
+            .Type = GW2Viewer::UI::Notification::Types::Error,
+            .Text = "Config save failed:\nUnable to write data to the file.",
+        });
+        return false;
+    }
+    G::Notifications.AddTimed(1s, {
+        .Type = GW2Viewer::UI::Notification::Types::Success,
+        .Text = "Config saved.",
+    });
 }
 
 }
